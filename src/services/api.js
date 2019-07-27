81eb14148ed679852129e7b0a13c8f176c0e5b54
Shaba9 <shaba92@gmail.com>
2019-07-27 15:46:29 -0700

    headers added to request

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,6 @@
 import { post } from './request';
 
 export const sendEmail = message => {
-  console.log('email env', process.env.EMAIL_USER);
   const mailOptions = {
     from: `${message.name} <${message.email}>`,
     to: process.env.EMAIL_USER,
